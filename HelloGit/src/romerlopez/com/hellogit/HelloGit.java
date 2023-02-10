24c7777e7ae3d7ddc2215645a9248259974b1d79
Visitor <Visitor@Marc-Laptop>
2023-02-10 13:57:13 -0500

    My First commit - Hwllo Git message

---
+++ HelloGit/src/romerlopez/com/hellogit/HelloGit.java
@@ -0,0 +1,12 @@
+package romerlopez.com.hellogit;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Hello Github");	
+
+	}
+
+}
